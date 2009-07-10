58b578970e7eb2058e1d81794251b0a743ccdfad
muzerakascooby <muzerakascooby@6fbaa20e-6d6e-11de-b692-19bc0a66ba1d>
2009-07-10 19:36:02 +0000

    Thread example

--- trunk/unstable/server/main.cpp
+++ trunk/unstable/server/main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <cstdlib>
+#include <pthread.h>
+#include <unistd.h>
 
 using namespace std;
 
+void *ping(){
+    while(1){
+        cout << "Ping running\n";
+        sleep(1);
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    pthread_t pingthrd;
+    int retval;
     if(argc != 1){
         cout << "Usage: " << argv[0] << endl;
         exit(1);
+    }
+    retval = pthread_create((pthread_t*)&pingthrd,NULL,(void* (*)(void*))ping,NULL);
+    while(1){
+        cout << "Main running\n";
+        sleep(2);
     }
     return 0;
 }
