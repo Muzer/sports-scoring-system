398f55392c542df36ff50aa647945227289710b5
muzerakascooby <muzerakascooby@6fbaa20e-6d6e-11de-b692-19bc0a66ba1d>
2009-07-10 18:14:49 +0000

    Added usage information if arguments were specified

--- trunk/unstable/server/main.cpp
+++ trunk/unstable/server/main.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
 int main(int argc, char *argv[])
 {
+    if(argc != 1){
+        cout << "Usage: " << argv[0] << endl;
+        exit(1);
+    }
     return 0;
 }
